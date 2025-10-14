35856958f35b4d947d0c3cde3658664fd88fac72
Alexander Minza <alexander.minza@gmail.com>
2025-10-14 22:12:35 +0300

    getToken createQr

--- src/MaibMia/MaibMiaClient.php
+++ src/MaibMia/MaibMiaClient.php
@@ -11,4 +11,47 @@ use GuzzleHttp\Exception\BadResponseException;
 
 class MaibMiaClient extends GuzzleClient
 {
+    const DEFAULT_BASE_URL = 'https://api.maibmerchants.md/';
+    const SANDBOX_BASE_URL = 'https://sandbox.maibmerchants.md/';
+
+    /**
+     * @param ClientInterface      $client
+     * @param DescriptionInterface $description
+     * @param array                $config
+     */
+    public function __construct(
+        ?ClientInterface $client = null,
+        ?DescriptionInterface $description = null,
+        array $config = []
+    ) {
+        $client = $client instanceof ClientInterface ? $client : new Client();
+        $description = $description instanceof DescriptionInterface ? $description : new MaibMiaDescription($config);
+        parent::__construct($client, $description, null, null, null, $config);
+    }
+
+    /**
+     * Obtain Authentication Token
+     * @link https://docs.maibmerchants.md/mia-qr-api/en/endpoints/authentication/obtain-authentication-token
+     */
+    public function getToken($clientId, $clientSecret)
+    {
+        $args = [
+            'clientId' => $clientId,
+            'clientSecret' => $clientSecret
+        ];
+
+        return parent::getToken($args);
+    }
+
+    public function createQr($qrData, $authToken)
+    {
+        $qrData['authToken'] = $authToken;
+
+        // $args = [
+        //     'qrData' => $qrData,
+        //     'authToken' => $authToken
+        // ];
+
+        return parent::createQr($qrData);
+    }
 }
--- src/MaibMia/MaibMiaDescription.php
+++ src/MaibMia/MaibMiaDescription.php
@@ -6,4 +6,69 @@ use GuzzleHttp\Command\Guzzle\Description;
 
 class MaibMiaDescription extends Description
 {
+    public function __construct(array $options = [])
+    {
+        $authorizationHeader = [
+            'type' => 'string',
+            'location' => 'header',
+            'sentAs' => 'Authorization',
+            'description' => 'Bearer Authentication with JWT Token',
+            'required' => true,
+        ];
+
+        $description = [
+            //'baseUrl' => 'https://api.maibmerchants.md/',
+            'name' => 'maib MIA QR API',
+            'apiVersion' => 'v2',
+
+            'operations' => [
+                // Authentication Operations
+                'getToken' => [
+                    'httpMethod' => 'POST',
+                    'uri' => '/v2/auth/token',
+                    'description' => 'Obtain Authentication Token',
+                    'responseModel' => 'getResponse',
+                    'parameters' => [
+                        'clientId' => ['type' => 'string', 'location' => 'json'],
+                        'clientSecret' => ['type' => 'string', 'location' => 'json'],
+                    ],
+                ],
+
+                // QR Operations
+                'createQr' => [
+                    'httpMethod' => 'POST',
+                    'uri' => '/v2/mia/qr',
+                    'description' => 'Create QR Code (Static, Dynamic)',
+                    'responseModel' => 'getResponse',
+                    'parameters' => [
+                        'authToken' => $authorizationHeader,
+                        // 'qrData' => ['location' => 'json'],
+                        'type' => ['type' => 'string', 'location' => 'json', 'enum' => ['Static', 'Dynamic'], 'required' => true],
+                        'expiresAt' => ['type' => 'string', 'format' => 'date-time', 'location' => 'json'],
+                        'amountType' => ['type' => 'string', 'location' => 'json', 'enum' => ['Fixed', 'Controlled', 'Free'], 'required' => true],
+                        'amount' => ['type' => 'number', 'location' => 'json'],
+                        'amountMin' => ['type' => 'number', 'location' => 'json'],
+                        'amountMax' => ['type' => 'number', 'location' => 'json'],
+                        'currency' => ['type' => 'string', 'location' => 'json', 'enum' => ['MDL'], 'required' => true],
+                        'description' => ['type' => 'string', 'location' => 'json', 'required' => true],
+                        'orderId' => ['type' => 'string', 'location' => 'json'],
+                        'callbackUrl' => ['type' => 'string', 'location' => 'json'],
+                        'redirectUrl' => ['type' => 'string', 'location' => 'json'],
+                        'terminalId' => ['type' => 'string', 'location' => 'json'],
+                    ],
+                ],
+            ],
+
+            'models' => [
+                'getResponse' => [
+                    'type' => 'object',
+                    'additionalProperties' => [
+                        'location' => 'json'
+                    ]
+                ]
+            ]
+        ];
+
+        parent::__construct($description, $options);
+    }
 }
