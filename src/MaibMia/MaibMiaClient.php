714ca40a0e8db323d1c69f1a37203b64f7cf5652
Alexander Minza <alexander.minza@gmail.com>
2025-10-14 17:15:44 +0300

    MaibMiaClient MaibMiaDescription

---
+++ src/MaibMia/MaibMiaClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Maib\MaibMia;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Command\Guzzle\DescriptionInterface;
+use GuzzleHttp\Command\Guzzle\GuzzleClient;
+use GuzzleHttp\Command\Result;
+use GuzzleHttp\Exception\BadResponseException;
+
+class MaibMiaClient extends GuzzleClient
+{
+}
---
+++ src/MaibMia/MaibMiaDescription.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Maib\MaibMia;
+
+use GuzzleHttp\Command\Guzzle\Description;
+
+class MaibMiaDescription extends Description
+{
+}
